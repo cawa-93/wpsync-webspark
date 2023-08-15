02634da9fe40f0ac5bd6415ea6646cb49e1ead28
Олександр Козак <kozackunisoft@gmail.com>
2023-08-15 12:25:17 +0300

    refactor

--- index.php
+++ index.php
@@ -5,8 +5,6 @@
  * Version: 0.0.1
  * Author: Олександр Козак
  * Author URI: https://kozack.me
- * Text Domain: woocommerce
- * Domain Path: /i18n/languages/
  * Requires at least: 6.3
  * Requires PHP: 8.1
  */
@@ -45,7 +43,6 @@ add_action( 'wpsync_cron_hook', 'wpsync\main_sync' );
  * @throws WC_Data_Exception
  */
 function main_sync(): void {
-
 	/**
 	 * @var $products WC_Product[]
 	 */
@@ -62,11 +59,12 @@ function main_sync(): void {
 		unset( $products_data[ $product->get_sku() ] );
 
 		if ( empty( $data ) ) {
+			$image_id = $product->get_image_id();
+			wp_delete_attachment( $image_id, true );
 			$product->delete( true );
-			continue;
+		} else {
+			put_product( $data, $product );
 		}
-
-		put_product( $data, $product );
 	}
 
 	// Add new products
@@ -77,24 +75,16 @@ function main_sync(): void {
 
 /**
  * Load product details
- *
  */
-function load_products() {
+function load_products( $attempt = 1 ) {
+	$response = json_decode( wp_remote_retrieve_body( wp_remote_get( 'https://wp.webspark.dev/wp-api/products' ) ) );
 
-	$ch      = curl_init();
-	$timeout = 5; // request timeout 5 second
+	if ( empty( $response ) || $response->error ) {
+		if ( $attempt >= 5 ) {
+			exit( 1 );
+		}
 
-	curl_setopt( $ch, CURLOPT_URL, 'https://wp.webspark.dev/wp-api/products' );
-	curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-	curl_setopt( $ch, CURLOPT_TIMEOUT, $timeout );
-	curl_setopt( $ch, CURLOPT_CONNECTTIMEOUT, $timeout );
-
-	$response = json_decode( curl_exec( $ch ) );
-	curl_close( $ch );
-
-
-	if ( empty($response) || $response->error ) {
-		exit( 1 );
+		return load_products( $attempt + 1 );
 	}
 
 	return array_reduce( $response->data, function ( $carry, $item ) {
@@ -105,7 +95,8 @@ function load_products() {
 }
 
 /**
- * Create product or update existing
+ * Creates a product or updates an existing one
+ *
  * @param $data
  * @param WC_Product $product
  *
@@ -113,14 +104,112 @@ function load_products() {
  * @throws WC_Data_Exception
  */
 function put_product( $data, WC_Product $product = new WC_Product_Simple() ): void {
+
 	$product->set_name( $data->name );
-	$product->set_price( $data->price );
-	$product->set_description( $data->description );
+
+	$product->set_description( html_entity_decode( $data->description ) );
+
 	$product->set_stock_quantity( $data->in_stock );
+
 	$product->set_sku( $data->sku );
+
+	$price = preg_replace( '/[^0-9.]/', '', $data->price );
+
+	$product->set_regular_price( $price );
+
+	if ( empty( $product->get_image_id() ) ) {
+
+		if ( empty( $product->get_id() ) ) {
+			$product->save();
+		}
+
+		$image_id = upload_image_from_url( $data->picture, $data->name, $product->get_id() );
+		if ( $image_id !== false ) {
+			$product->set_image_id( $image_id );
+		}
+	}
+
 	$product->save();
 }
 
 
+/**
+ * Upload a file to the media library using a URL.
+ *
+ * @param string $url URL to be uploaded
+ * @param string|null $title If set, used as the post_title
+ * @param int|string|null $post_id The post ID the media is associated with.
+ *
+ * @return int|false
+ * @version 1.2
+ *
+ * @see https://gist.github.com/RadGH/966f8c756c5e142a5f489e86e751eacb
+ */
+function upload_image_from_url( string $url, string $title = null, int|string|null $post_id = 0 ) {
+	require_once( ABSPATH . "/wp-load.php" );
+	require_once( ABSPATH . "/wp-admin/includes/image.php" );
+	require_once( ABSPATH . "/wp-admin/includes/file.php" );
+	require_once( ABSPATH . "/wp-admin/includes/media.php" );
 
+	// Download url to a temp file
+	$tmp = download_url( $url );
+	if ( is_wp_error( $tmp ) ) {
+		return false;
+	}
+
+	// Get the filename and extension ("photo.png" => "photo", "png")
+	$filename  = pathinfo( $url, PATHINFO_FILENAME );
+	$extension = pathinfo( $url, PATHINFO_EXTENSION );
+
+	// An extension is required or else WordPress will reject the upload
+	if ( ! $extension ) {
+		// Look up mime type, example: "/photo.png" -> "image/png"
+		$mime = mime_content_type( $tmp );
+		$mime = is_string( $mime ) ? sanitize_mime_type( $mime ) : false;
+
+		// Only allow certain mime types because mime types do not always end in a valid extension (see the .doc example below)
+		$mime_extensions = array(
+			// mime_type         => extension (no period)
+			'text/plain'         => 'txt',
+			'text/csv'           => 'csv',
+			'application/msword' => 'doc',
+			'image/jpg'          => 'jpg',
+			'image/jpeg'         => 'jpeg',
+			'image/gif'          => 'gif',
+			'image/png'          => 'png',
+			'video/mp4'          => 'mp4',
+		);
+
+		if ( isset( $mime_extensions[ $mime ] ) ) {
+			// Use the mapped extension
+			$extension = $mime_extensions[ $mime ];
+		} else {
+			// Could not identify extension
+			@unlink( $tmp );
+
+			return false;
+		}
+	}
+
+
+	// Upload by "sideloading": "the same way as an uploaded file is handled by media_handle_upload"
+	$args = array(
+		'name'     => "$filename.$extension",
+		'tmp_name' => $tmp,
+	);
+
+	// Do the upload
+	$attachment_id = media_handle_sideload( $args, $post_id, $title );
+
+	// Cleanup temp file
+	@unlink( $tmp );
+
+	// Error uploading
+	if ( is_wp_error( $attachment_id ) ) {
+		return false;
+	}
+
+	// Success, return attachment ID (int)
+	return (int) $attachment_id;
+}
 
